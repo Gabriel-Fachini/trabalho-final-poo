0b1b733f7b30894ebac8109148ed950b622968e4
Gustavo Sampaio <gustavo.sampaio@usp.br>
2022-07-22 14:40:20 -0300

    Finalizacao da votacao na urna

--- src/arquivos/Urna.py
+++ src/arquivos/Urna.py
@@ -8,7 +8,7 @@ class Urna():
         roda_urna(self, window)
     
 def roda_urna(urna, window):
-    iteracao_urna_quiz(window, 0)
+    iteracao_urna_menu(window, 0)
     
     voto = ''
     aux = 0
@@ -18,33 +18,31 @@ def roda_urna(urna, window):
         print(eventos, valores)
 
         if eventos == '-CONFIRMA-':
-            print("Entrei if 1")
             voto = valores['-NUMERO_CANDIDATO-1']
             if verificacaoVoto(voto) == False:
-                print("Entrei if 2")
                 window['-ERRO_VOTO-'].update('Insira apenas números!', background_color='red', visible=True)
 
             elif verificaNumeroCandidatoExiste(urna.lista_candidatos, int(voto)) == False:
-                print("Entrei if 3")
                 window['-ERRO_VOTO-'].update('Candidato não encontrado!', background_color='red', visible=True)
 
             else:
-                print("Entrei if 4")
+                candidato = verificaNumeroCandidatoExiste(urna.lista_candidatos, int(voto))
+                window['-NOME_CANDIDATO_FINAL-'].update(candidato.nome)
+                window['-IDADE_CANDIDATO_FINAL-'].update(candidato.numero)
+                window['-P1_CANDIDATO_URNA_FINAL-'].update(candidato.propostas[0])
+                window['-P2_CANDIDATO_URNA_FINAL-'].update(candidato.propostas[1])
+                window['-P3_CANDIDATO_URNA_FINAL-'].update(candidato.propostas[2])
                 window['-URNA_LY-'].update(visible=False)
-                window['-VOTO_CONFIRMADO-'].update(valores['-NUMERO_CANDIDATO-1'])
                 window['-URNA_LY_FIM-'].update(visible=True)
                 
         if eventos == '-VE_CANDIDATO-':
             voto = valores['-NUMERO_CANDIDATO-1']
             if verificacaoVoto(voto) == False:
-                print("Entrei if 2")
                 window['-ERRO_VOTO-'].update('Insira apenas números!', background_color='red', visible=True)
             elif verificaNumeroCandidatoExiste(urna.lista_candidatos, int(voto)) == False:
-                print("Entrei if 3")
                 window['-ERRO_VOTO-'].update('Candidato não encontrado!', background_color='red', visible=True)
             else:
                 candidato = verificaNumeroCandidatoExiste(urna.lista_candidatos, int(voto))
-                print("Entrei if 9")
                 window['-ERRO_VOTO-'].update(visible=False)
                 atualiza_candidato(window, 0)
                 window['-NOME_CANDIDATO_URNA-'].update(candidato.nome)
@@ -53,10 +51,12 @@ def roda_urna(urna, window):
                 window['-P1_CANDIDATO_URNA-'].update(candidato.propostas[0])
                 window['-P2_CANDIDATO_URNA-'].update(candidato.propostas[1])
                 window['-P3_CANDIDATO_URNA-'].update(candidato.propostas[2])
+                
+        if eventos == '-VOLTA_URNA-':
+            iteracao_urna_candidato(window)
                     
-        if eventos == '-VOLTA_MENU-' or eventos =='-VOLTA_MENU-3' or eventos == '-VOLTA_MENU-4':
-            print("Entrei if 5")
-            iteracao_urna_quiz(window, 1)
+        if eventos == '-VOLTA_MENU-' or eventos =='-VOLTA_MENU-3' or eventos == '-VOLTA_MENU-4' or eventos == '-VOLTA_MENU-5':
+            iteracao_urna_menu(window, 1)
             break
 
         if eventos == sg.WINDOW_CLOSED:
@@ -77,7 +77,7 @@ def verificaNumeroCandidatoExiste(listaCandidatos, voto):
   return False
 
 
-def iteracao_urna_quiz(window, troca):
+def iteracao_urna_menu(window, troca):
     if troca == 0:
         window['-MENU_LY-'].update(visible=False)
         window['-URNA_LY-'].update(visible=True)
@@ -86,6 +86,10 @@ def iteracao_urna_quiz(window, troca):
         window['-URNA_LY-'].update(visible=False)
         window['-URNA_LY_FIM-'].update(visible=False)
         window['-URNA_LY_CANDIDATO-'].update(visible=False)
+        
+def iteracao_urna_candidato(window):
+    window['-URNA_LY_CANDIDATO-'].update(visible=False)
+    window['-URNA_LY-'].update(visible=True)
         
 def atualiza_candidato(window, troca):
     if troca == 0:
--- src/arquivos/main.py
+++ src/arquivos/main.py
@@ -8,7 +8,7 @@ TAMANHO_DE_TELA_MENU = (1190, 700)
 TAMANHO_DE_TELA_QUIZ = (900, 350)
 TAMANHO_DE_TELA_QUIZ_FINAL = (900, 450)
 TAMANHO_DE_TELA_URNA = (800, 600)
-TAMANHO_DE_TELA_URNA_CANDIDATO = (1000, 600)
+TAMANHO_DE_TELA_URNA_CANDIDATO = (1200, 700)
 
 sg.theme('GreenMono')
 
@@ -29,7 +29,7 @@ def main():
     layout_quiz_fim = [[sg.Text("Você deu match com: ", font="Courier 30", size=(None, 1)), sg.Text("", font="Courier 30", size=(None, 1), key='-NOME_CANDIDATO-')],
                     [sg.Text("", font="Courier 20", size =(55,6), key='-TEXTO_CANDIDATO-'), sg.Text("VOTE", font="Courier 25", size =(None,1))],
                     [sg.Text("Para apoiar, vote:", font="Courier 25", size =(None,1)), sg.Text("", font="Courier 25", size =(None,1), key='-NUMERO_CANDIDATO-')],
-                    [sg.Button('VOLTAR AO MENU PRINCIPAL', key='-VOLTA_MENU-', font="Courier 24", auto_size_button=True, button_color='#3065ac'), sg.Image(source="", size=(50,70),key='-FOTO_CANDIDATO-')],
+                    [sg.Button('VOLTAR AO MENU PRINCIPAL', key='-VOLTA_MENU-', font="Courier 24", auto_size_button=True, button_color='#3065ac'), sg.Image(source="", size=(50,10),key='-FOTO_CANDIDATO-')],
                     [sg.Text("", size=(None, 1))]]
     
     layout_urna = [
@@ -44,24 +44,30 @@ def main():
       [sg.Text("", size=(None, 2))]
     ]
     
-    layout_urna_candidato = [[sg.Text("Este candidato é: ", font="Courier 30", size=(None, 1)), sg.Text("", font="Courier 30", size=(None, 1), key='-NOME_CANDIDATO_URNA-')],
-                    [sg.Text("Idade = ", font="Courier 20"), sg.Text("", font="Courier 20", key='-IDADE_CANDIDATO_URNA-')],
-                    [sg.Text("Numero = ", font="Courier 20"), sg.Text("", font="Courier 20", key='-NUMERO_CANDIDATO_URNA-')],
-                    [sg.Text("Proposta 1 = ", font="Courier 20"), sg.Text("", font="Courier 20", key='-P1_CANDIDATO_URNA-')],
-                    [sg.Text("Proposta 2 = ", font="Courier 20"), sg.Text("", font="Courier 20", key='-P2_CANDIDATO_URNA-')],
-                    [sg.Text("Proposta 3 = ", font="Courier 20"), sg.Text("", font="Courier 20", key='-P3_CANDIDATO_URNA-')],
-                    [sg.Button] #fazendo botao para voltar do candidato para a urna
-                    [sg.Button('VOLTAR AO MENU PRINCIPAL', key='-VOLTA_MENU-', font="Courier 24", auto_size_button=True, button_color='#3065ac')],
-                    [sg.Text("", size=(None, 1))]]
+    layout_urna_candidato = [[sg.Text("Este candidato é: ", font="Courier 30", size=(None, 2)), sg.Text("", font="Courier 30", size=(17, 2), key='-NOME_CANDIDATO_URNA-')],
+                    [sg.Text("Idade = ", font="Courier 25", size=(8, 2)), sg.Text("", font="Courier 25", key='-IDADE_CANDIDATO_URNA-',size=(2, 2))],
+                    [sg.Text("Numero = ", font="Courier 25", size=(9, 2)), sg.Text("", font="Courier 25", key='-NUMERO_CANDIDATO_URNA-',size=(2, 2))],
+                    [sg.Text("Proposta 1 = ", font="Courier 25", size=(13, 2)), sg.Text("", font="Courier 25", key='-P1_CANDIDATO_URNA-', size=(45, 2))],
+                    [sg.Text("Proposta 2 = ", font="Courier 25", size=(13, 2)), sg.Text("", font="Courier 25", key='-P2_CANDIDATO_URNA-', size=(45, 2))],
+                    [sg.Text("Proposta 3 = ", font="Courier 25", size=(13, 2)), sg.Text("", font="Courier 25", key='-P3_CANDIDATO_URNA-', size=(45, 2))],
+                    #[sg.Button] #fazendo botao para voltar do candidato para a urna
+                    [sg.Button("VOLTAR PARA A URNA", font="Courier 24", size=(18,1) ,key="-VOLTA_URNA-")],
+                    [sg.Button('VOLTAR AO MENU PRINCIPAL', key='-VOLTA_MENU-', font="Courier 24", auto_size_button=True, button_color='#3065ac')]
+                    ]
     
     layout_urna_confirmacao = [
-        [sg.Text('Votação finalizada', font="Courier 24")],
+        [sg.Text('Voto armazenado com sucesso!', font="Courier 35", justification='center')],
         [sg.Text("", size=(None, 2))],
-        [sg.Text('', key='-VOTO_CONFIRMADO-', font="Courier 48")],
+        [sg.Text("Candidato: ", font="Courier 25"), sg.Text("", font="Courier 25", key="-NOME_CANDIDATO_FINAL-")],
         [sg.Text("", size=(None, 2))],
-        [sg.Button('VOLTAR AO MENU PRINCIPAL', key='-VOLTA_MENU-', font="Courier 24")],
-        [sg.Text("", size=(None, 2))]
-    ]
+        [sg.Text("Idade: ", font="Courier 25"), sg.Text('', key='-IDADE_CANDIDATO_FINAL-', font="Courier 25")],
+        [sg.Text("", size=(None, 2))],
+        [sg.Text("Proposta 1 = ", font="Courier 25", size=(13, 2)), sg.Text("", font="Courier 25", key='-P1_CANDIDATO_URNA_FINAL-', size=(45, 2))],
+        [sg.Text("Proposta 2 = ", font="Courier 25", size=(13, 2)), sg.Text("", font="Courier 25", key='-P2_CANDIDATO_URNA_FINAL-', size=(45, 2))],
+        [sg.Text("Proposta 3 = ", font="Courier 25", size=(13, 2)), sg.Text("", font="Courier 25", key='-P3_CANDIDATO_URNA_FINAL-', size=(45, 2))],
+        [sg.Text("", size=(None, 2))],
+        [sg.Button('VOLTAR AO MENU PRINCIPAL', key='-VOLTA_MENU-', font="Courier 24", button_color='#3065ac')]
+        ]
     
     #layout_simulacao = []
     
@@ -69,7 +75,7 @@ def main():
                             [sg.Text("-- Instruções de uso --", font="Courier 25", size =(None,2))],
                             [sg.Text("Aperte em 'URNA REAL' para simular uma votação real para o próximo", font="Courier 20", size =(None,1))],
                             [sg.Text("presidente do Brasil.", font="Courier 20", size =(None,2))],
-                            [sg.Text("Aperte em 'SIMULAÇÃO DE VOTOS' para simular votos de 5 cidades diferentes", font="Courier 20", size =(None,1))],
+                            [sg.Text("Aperte em 'SIMULAÇÃO DE VOTOS' para simular 1 MILHÃO de votos", font="Courier 20", size =(None,1))],
                             [sg.Text("aleatoriamente de maneira automática.", font="Courier 20", size =(None,2))],
                             [sg.Text("Aperte em 'QUIZ' para responder a um questionário que, ao final, indicará", font="Courier 20", size =(None,1))],
                             [sg.Text("qual o candidato que mais combina com você", font="Courier 20", size =(None,3))],
@@ -84,31 +90,31 @@ def main():
                sg.pin(sg.Column(layout_quiz_fim, key='-QUIZ_LY_FIM-', size=TAMANHO_DE_TELA_QUIZ_FINAL, element_justification='left', visible=False, expand_x=True, expand_y=True)),
                sg.pin(sg.Column(layout_urna, key='-URNA_LY-', size=TAMANHO_DE_TELA_URNA, element_justification='center', visible=False, expand_x=True, expand_y=True)),
                sg.pin(sg.Column(layout_urna_candidato, key='-URNA_LY_CANDIDATO-', size=TAMANHO_DE_TELA_URNA_CANDIDATO, element_justification='left', visible=False, expand_x=True, expand_y=True)),
-               sg.pin(sg.Column(layout_urna_confirmacao, key='-URNA_LY_FIM-', size=TAMANHO_DE_TELA_URNA, element_justification='center', visible=False, expand_x=True, expand_y=True))]]
+               sg.pin(sg.Column(layout_urna_confirmacao, key='-URNA_LY_FIM-', size=TAMANHO_DE_TELA_URNA_CANDIDATO, visible=False, expand_x=True, expand_y=True))]]
                #sg.pin(sg.Column(layout_simulacao, key='-SIMULACAO_LY-', size=TAMANHO_DE_TELA, element_justification='center', visible=False))]]
 
     window = sg.Window('Simulador de Urna Eletrônica', layout, finalize=True)
     
     #Criacao de candidatos
     candidato1 = Candidato("Jessica Matos", 35, "Partido dos Negacionistas Gigantes (PNG)", 14, 
-                           ["Acabar com faculdades públicas", 
-                            "Vacinas não poderão ser exigidas em locais públicos e/ou privados", 
-                            "Apoio federal para pessoas com mais de 1.95 de altura"])
+                           ["Acabar com faculdades públicas.", 
+                            "Vacinas não poderão ser exigidas em locais públicos e/ou privados.", 
+                            "Apoio federal para pessoas com mais de 1.95 de altura."])
     
     candidato2 = Candidato("Matheus Bragança", 43, "Partido dos Lavadores Aquafóbicos (PLA)", 19,
-                           ["Diminuição de impostos nas lavagens a seco",
-                            "Aumento dos impostos na água",
-                            "Multa de R$3523,24 para quem for flagrado tomando mais de um banho por semana"])
+                           ["Diminuição de impostos nas lavagens a seco.",
+                            "Aumento dos impostos na água.",
+                            "Multa de R$3523,24 para quem for flagrado tomando mais de um banho por semana."])
     
     candidato3 = Candidato("Jefferson Moreira", 42, "Partido dos Adoradores de Tênis de Mesa (PATM)", 12,
-                           ["Tênis de mesa em cada escola pública é lei",
-                            "Bolsa Raquete para cargos públicos", 
-                            "Taxas de importação de madeira caem 49%"])
+                           ["Tênis de mesa em cada escola pública é lei.",
+                            "Bolsa Raquete para cargos públicos.", 
+                            "Taxas de importação de madeira caem 49%."])
     
     candidato4 = Candidato("Roberto Autônomo", 51, "Partido dos Idealizadores Atômicos (PIA)", 16,
-                           ["Brasil entrará no Tratado Urgente para Proliferação de Armas Nucleares (TUPAN) imediatamente",
-                            "Investimento de 78%/ do PIB Brasileiro em armas nucleares",
-                            "Curso básico de atomística será exigido para entrada na escola infantil e fundamental"])
+                           ["Brasil entrará no Tratado de Proliferação de Armas Nucleares (TPAN) imediatamente.",
+                            "Investimento de 78%/ do PIB Brasileiro em armas nucleares.",
+                            "Curso básico de atomística será exigido para entrada na escola infantil e fundamental."])
     
     start = datetime.datetime.now()
     last_post_read_time = start
@@ -125,6 +131,8 @@ def main():
             Quiz(candidato1, candidato2, candidato3, candidato4, window)
         elif event == '-VOTO_UNICO-':
             Urna(candidato1, candidato2, candidato3, candidato4, window)
+        if event == '-SIMULACAO-':
+            pass
         elif event == sg.WIN_CLOSED or event == '-QUIT-':
             break
         
